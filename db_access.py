cc5036ddff396d323f8b485c66b6f9c61ddaa105
Varun Vora <varunvora@live.com>
2018-11-02 01:51:16 +0530

    fixed login for 'username does not exist'

--- db_access.py
+++ db_access.py
@@ -1,3 +1,9 @@
+"""
+This file contains the code that connects the backend to database
+Run this file to check if the connection to database works
+If it doesn't throw an error, it works!
+"""
+
 import psycopg2
 import logging
 
@@ -28,6 +34,7 @@ Referenced by:
     TABLE "question" CONSTRAINT "question_p_id_fkey" FOREIGN KEY (p_id) REFERENCES professor(p_id)
 """
 
+
 def connect_db():
     """
     Connects to the postgres database
@@ -42,8 +49,15 @@ def connect_db():
         logging.error('Failed to connect to database')
         return None
 
+
 def validate_student(usn, password):
-    query = """SELECT (SELECT password FROM student where usn = '""" + usn + """') = '""" + password + """';"""
+    """
+    Validates login credential for student
+    :param usn: student's usn, e.g. 01FB15ECS342
+    :param password: student's password, e.g. 01FB15ECS342
+    :return: False if usn does not exist or password doesn't match. Else True
+    """
+    query = """SELECT (SELECT '""" + usn + """' IN (SELECT usn FROM student)) AND (SELECT (SELECT password FROM student where usn = '""" + usn + """') = '""" + password + """');"""
     conn = connect_db()
     res = False
     if conn:
@@ -53,8 +67,15 @@ def validate_student(usn, password):
         conn.close()
     return res
 
-def validate_professor(a_id, password):
-    query = """SELECT (SELECT password FROM professor where a_id = '""" + a_id + """') = '""" + password + """';"""
+
+def validate_professor(p_id, password):
+    """
+    Validates login credential for professor
+    :param p_id: professor's id, e.g. 01FB15ECS342
+    :param password: professor's password, e.g. 01FB15ECS342
+    :return: False if p_id does not exist or password doesn't match. Else True
+    """
+    query = """SELECT (SELECT '""" + p_id + """' IN (SELECT p_id FROM professor)) AND (SELECT (SELECT password FROM professor where p_id = '""" + p_id + """') = '""" + password + """');"""
     conn = connect_db()
     res = False
     if conn:
@@ -63,8 +84,7 @@ def validate_professor(a_id, password):
         res = cursor.fetchall()[0][0]
         conn.close()
     return res
-
 
 
 if __name__ == "__main__":
-    print(validate_student('01FB16ECS249', '01FB15ECS342'))
+    print(validate_professor('abcd', 'pqrs'))
