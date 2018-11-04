eab8cfbb299a937a7b20755d4ce17857e8d7dfd5
root <root@LAPTOP-48S78VBJ.localdomain>
2018-11-04 20:19:31 +0530

    better dashboard

--- contest_info.py
+++ contest_info.py
@@ -1,17 +1,17 @@
-from db_access import get_active_contest_student, get_archived_contest_student
+from db_access import get_active_contest, get_archived_contest
 from flask import render_template
 
 
 def contest_list(usn):
 
-	#active_contests = get_active_contest_student(usn)
-	#archivef_contests = get_archived_contest_student(usn)
+	#active_contests = get_active_contest(usn)
+	#archivef_contests = get_archived_contest(usn)
 	active_contests = [{'name': "kys", 'time': "now", 'active': 1},{'name': "gabe", 'time': "now", 'active': 1}]
 	archived_contests = [{'name': "kys1", 'time': "now", 'active': 0},{'name': "gabe1", 'time': "now", 'active': 0}]
-	contests = active_contests + archived_contests
+	#contests = active_contests + archived_contests
 	
 	
-	return render_template("Student Dashboard.html", contests = contests)
+	return render_template("Student Dashboard.html", active_contests = active_contests, archived_contests = archived_contests)
 	
 
 
