f20a1a45bcfdad83cf8af9997fc7ae0c070420f5
Vivek-kapoor <vivek.kapoor.2959@gmail.com>
2018-11-05 14:20:57 +0530

    integration with backend code

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
+# import os
+# from flask import Flask, render_template, request
+# from db_access import validate_student, validate_professor
+# from contest_info import contest_list
 import os
-from flask import Flask, render_template, request
-from db_access import validate_student, validate_professor
-from contest_info import contest_list
-
-SESSION = dict()
+from flask import Flask
+from routes import *
+# SESSION = dict()
 
 app = Flask(__name__)
 app._static_folder = os.path.join(os.getcwd(),"static")
-
+app.config['SECRET_KEY'] = '59d3ca27e6701d3fd06eb960ca5866a5'
 
 @app.route('/')
 def codestreak():
-	return render_template("login.html")
+	return route_codestreak()
 
-
-
-@app.route('/student_login',methods = ['POST', 'GET'])
-
+@app.route("/student_login", methods=["GET", "POST"])
 def student_login():
+	return route_student_login()
 
-	if request.method == 'POST':
-		print("here")
-		usn = request.form['usn']
-		password = request.form['password']
+@app.route("/prof_login", methods=["GET", "POST"])
+def prof_login():
+	return route_prof_login()
 
-		if validate_student(usn, password):
-			return contest_list(usn)
-		else:
-			return render_template("check.html")
+@app.route("/create_assignment", methods=["GET", "POST"])
+def create_assignment():
+	return route_create_assignment()
 
+@app.route("/add_questions", methods=["GET", "POST"])
+def add_questions():
+	return route_add_questions()
 
+if (__name__ == "__main__"):
+    app.run(debug=True)
+# @app.route('/student_login',methods = ['POST', 'GET'])
+# def student_login():
 
+# 	if request.method == 'POST':
+# 		print("here")
+# 		usn = request.form['usn']
+# 		password = request.form['password']
 
-
-if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-
+# 		if validate_student(usn, password):
+# 			return contest_list(usn)
+# 		else:
+# 			return render_template("check.html")
